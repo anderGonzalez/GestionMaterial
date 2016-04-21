e7df269337d8da863b48077e889f7a891b3d7776
Ander Gonzalez <andergonzaleztome@gmail.com>
2016-04-21 22:25:40 +0200

    Mierdas del mail y algo de la rutina de las devoluciones

---
+++ src/servicioPenalizaciones/CheckerDeoluciones.java
@@ -0,0 +1,10 @@
+package servicioPenalizaciones;
+
+public class CheckerDeoluciones {
+
+	public static void check() {
+
+		
+	}
+
+}
---
+++ src/servicioPenalizaciones/CheckerDevolucionesRutina.java
@@ -0,0 +1,33 @@
+package servicioPenalizaciones;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class CheckerDevolucionesRutina {
+	static final int MIN_TO_MILIS = 60000;
+	long periodo;	
+	Timer rutina;
+	
+	public CheckerDevolucionesRutina(long periodo){
+		this.periodo = periodo;
+		startRutina();
+	}
+	
+	public void startRutina(){
+		
+		rutina = new Timer();
+		rutina.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				
+				CheckerDeoluciones.check();
+				
+			}
+		}, 0, periodo * MIN_TO_MILIS );
+	}
+	
+	public void stopRutina(){
+		rutina.cancel();
+	}
+}
